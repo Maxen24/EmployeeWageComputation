24a22b1fd894c703c1da5a612bfbe0662923c979
Maxen24 <mshubham242@gmail.com>
2022-09-06 10:06:55 +0530

    Added : Employee is present or not

---
+++ src/com/bridgelabz/EmployeeWage.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package com.bridgelabz;
+
+/**
+ * @author mshub
+ *
+ */
+import java.lang.Math;
+public class EmployeeWage {
+
+	/**
+	 * @param args
+	 */
+	public void employeeIsPresentOrNot() {
+		
+	}
+	public static void main(String[] args) {
+		//Driving Code
+		double a= Math.random();
+		double Ispresent=0.5;
+		if (a>=Ispresent) {
+			System.out.println("Employee is present");
+		}else {
+			System.out.println("Employee is absent");
+		}
+		
+
+	}
+
+}
