6238f230acf942a81fac6707a2c89a3262bf101d
Maxen24 <mshubham242@gmail.com>
2022-09-07 09:24:46 +0530

    Added UC3 : Part time Employee Wage

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -22,18 +22,23 @@ public class EmployeeWage {
 		System.out.println("Welcome to Employee Wage Computation Program");
 	      
 		Random random= new Random();
-		int attendance=random.nextInt(2);
+		int attendance=random.nextInt(3);
 		int WagePerHour=20;
 		int FullDay=8;
 		int DailyWage=0;
+		int PartTime=4;
 		
 		if(attendance == 1) {
 			System.out.println("Employee Present..");
 			DailyWage=WagePerHour*FullDay;
 			System.out.println("Daily Employee Wage is "+" "+DailyWage);
+		}	else if (attendance == 2) {
+			System.out.println("Employee Present..");
+			DailyWage=WagePerHour*PartTime;
+			System.out.println("Parttime Employee Wage is "+" "+DailyWage);
+			
 		}else
 			System.out.println("Employee Absent..");
-		System.out.println("Daily Employee Wage is "+" "+DailyWage);
 		
 	}
 
